85f21e366f1c128c509784a98226db295165bcbd
naoto-web <kj.naoto@gmail.com>
2026-08-29 10:15:06 +0900

    SUB_FIXEDを既定OFFへ（カメラを1368/544へ修正済みとNaoto確認・8/29）

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260829-055314";
+  var AU_BUILD = "20260829-101401";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
