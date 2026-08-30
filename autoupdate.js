67f810c53efa13b613b3db5428b2f96fc4428899
naoto-web <kj.naoto@gmail.com>
2026-08-30 16:59:56 +0900

    FB130補修 アジャスト④の下の歯を白に（ポケット除去の誤認をMOUTH_BOXで修正・adj1/adj4再生成）

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260830-033620";
+  var AU_BUILD = "20260830-165901";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
