561d557169c281d313eec485a1a719014c969771
naoto-web <kj.naoto@gmail.com>
2026-08-30 21:53:30 +0900

    FB151補修 ②レース観戦を全画面演出の対象から除外（レース映像ソースがオーバーレイより上＝裏に隠れる・8/30実測）

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260830-165901";
+  var AU_BUILD = "20260830-215234";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
