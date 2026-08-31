733b7bc4bfc2231d36134352c5d74433135d658f
naoto-web <kj.naoto@gmail.com>
2026-08-31 20:51:59 +0900

    update

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260831-203918";
+  var AU_BUILD = "20260831-205100";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
