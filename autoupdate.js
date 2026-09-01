8165d51131f61b1802f0710eebf6cfb569a7a9be
naoto-web <kj.naoto@gmail.com>
2026-09-02 02:07:38 +0900

    hitouch: recut pose 10 at measured palm seam (yellow had partner's hand baked in, red's hand was clipped)

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260902-005449";
+  var AU_BUILD = "20260902-020625";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
