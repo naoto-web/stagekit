057c2fd72b094b08fff6e0b1af1359151e12153b
naoto-web <kj.naoto@gmail.com>
2026-09-03 15:14:42 +0900

    ori (orichalcum) trigger threshold: odds <3 -> <5 (LOWODDS_MAX)

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260902-171740";
+  var AU_BUILD = "20260903-151326";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
