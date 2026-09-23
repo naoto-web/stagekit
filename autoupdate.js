1a9084714b02e8c5606ab067749dbed1860eaa96
naoto-web <kj.naoto@gmail.com>
2026-09-23 11:28:41 +0900

    riders: 追走能力の根拠欄を全幅に（書くの位置をS取りとそろえる）

--- autoupdate.js
+++ autoupdate.js
@@ -24,7 +24,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260923-111511";
+  var AU_BUILD = "20260923-112702";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
