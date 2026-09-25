48811a7582730add130fb4c843c3c02a810608f9
naoto-web <kj.naoto@gmail.com>
2026-09-26 06:56:58 +0900

    本日設定の並びを 配信者→本日の場→note勝負レース に（9/26 Naoto）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-064613";
+  var AU_BUILD = "20260926-065446";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
