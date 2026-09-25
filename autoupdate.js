654b4e83a3bfeaa4f5978c2649607b8791cc6250
naoto-web <kj.naoto@gmail.com>
2026-09-25 10:09:43 +0900

    overlay: ②③の空席の出走表は名前と下の行の間あけを戻す（①だけあける）（9/25 Naoto）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260925-100017";
+  var AU_BUILD = "20260925-100735";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
