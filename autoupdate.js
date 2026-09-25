add33e2e99e19ad1224b1724b0f97713c52802e6
naoto-web <kj.naoto@gmail.com>
2026-09-26 03:28:32 +0900

    note予想の燃える枠を既定OFFに（&nfire=1で復帰）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-032008";
+  var AU_BUILD = "20260926-032611";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
