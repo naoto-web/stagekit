7720ec93ae7faeda8ec3053cc428894e89f5a1b1
naoto-web <kj.naoto@gmail.com>
2026-09-26 07:13:36 +0900

    プラスの間の回収額（金）の黒フチを太く＝黄色の見出しでも読める（C案・9/26 Naoto）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-065446";
+  var AU_BUILD = "20260926-071127";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
