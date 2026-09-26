f0c64e00f705f9fdaa6d7b031c9d9c88794b9498
naoto-web <kj.naoto@gmail.com>
2026-09-26 21:30:26 +0900

    締切のハンコ：枠と字の余白を字の大きさに比例・3〜4場は枠線2px・2場は字26pxで行の高さを維持（9/26 Naoto「つぶれて見える」）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-210734";
+  var AU_BUILD = "20260926-212813";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
