82a054d61b16213fecf8e928691929215581f80c
naoto-web <kj.naoto@gmail.com>
2026-09-26 18:50:33 +0900

    オッズ確定後の数字を従来の平らな金色（#c28400）に戻す＝金属グラデ・薄い影をやめる（キランは維持・9/26 Naoto）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-183904";
+  var AU_BUILD = "20260926-184829";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
