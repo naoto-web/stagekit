e831893e17dc347e3aa79f7f4a96ae64d36dc8ff
naoto-web <kj.naoto@gmail.com>
2026-09-26 21:09:38 +0900

    3場タイマー（②③）：ラベル20→18px＝ラベルと数字の間に隙間（9/26 Naoto「潰れて見える」）

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,7 @@
 
 (function () {
   "use strict";
-  var AU_BUILD = "20260926-205337";
+  var AU_BUILD = "20260926-210734";
 
   var params;
   try { params = new URLSearchParams(location.search); } catch (e) { return; }
